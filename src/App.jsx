b657b6acb7ddf310d5f255270007c15e5e7100bc
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-09-19 22:05:45 +0530

    Rock-Paper-Scissors functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { FaHandRock, FaHandScissors, FaHandPaper } from 'react-icons/fa';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [userSelection, setuserSelection] = useState([]);
+  const [computerSelection, setComputerSelection] = useState([]);
+  const [status, setStatus] = useState(null);
+  const options = [
+    { component: <FaHandRock />, key: 'A', keyCode: 65, name: 'Rock' },
+    { component: <FaHandPaper />, key: 'S', keyCode: 83, name: 'Paper' },
+    { component: <FaHandScissors />, key: 'D', keyCode: 68, name: 'Scissors' },
+  ];
+
+  useEffect(() => {
+    switch (userSelection.name + computerSelection.name) {
+      case 'PaperRock':
+      case 'RockScissors':
+      case 'ScissorsPaper':
+        setStatus('Point for you');
+        break;
+      case 'RockPaper':
+      case 'ScissorsRock':
+      case 'PaperScissors':
+        setStatus('Point for the computer');
+        break;
+      case 'RockRock':
+      case 'ScissorsScissors':
+      case 'PaperPaper':
+        setStatus('Try again');
+        break;
+      default:
+        setStatus('');
+        break;
+    }
+    // ...
+  }, [userSelection, computerSelection]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleClick = option => {
+    generateComputerSelection();
+    setuserSelection(option);
+  };
+
+  const handleKeyDown = e => {
+    const option = options.filter(option => option.keyCode === e.keyCode);
+    if (!option.length) return;
+    handleClick(option[0]);
+  };
+
+  const generateComputerSelection = () => {
+    const computerSelection =
+      options[Math.floor(Math.random() * options.length)];
+    setComputerSelection(computerSelection);
+  };
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='selection'>Your selection: {userSelection.component}</div>
+      <div className='selection'>
+        Computer selection: {computerSelection.component}
+      </div>
+      <div className='selection'>{status}</div>
+      {options.map(option => (
+        <button
+          key={option.key}
+          title={`Press '${option.key}'`}
+          value={option}
+          onClick={() => handleClick(option)}>
+          {option.name} ({option.component})
+        </button>
+      ))}
+    </>
   );
-}
+};
 
 export default App;
